84917655934c8a9359cb9868059a8581dbd56669
onoyama-e-rikuo <evang00dd@gmail.com>
2020-06-11 19:56:23 +0900

    サービス

--- src/main/java/app/service/EmployeeService.java
+++ src/main/java/app/service/EmployeeService.java
@@ -1,5 +1,20 @@
 package app.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.entity.Employee;
+import app.repository.EmployeeRepository;
+
+@Service
 public class EmployeeService {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	public List<Employee> findAll() {
+		return employeeRepository.findAll();
+	}
 }
