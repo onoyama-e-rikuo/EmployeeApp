4b93c57f65f3bcac9b2567cb6616f4108264f3fc
onoyama-e-rikuo <evang00dd@gmail.com>
2020-06-12 11:24:32 +0900

    dto中身

--- src/main/java/app/dto/EmployeeRequest.java
+++ src/main/java/app/dto/EmployeeRequest.java
@@ -1,7 +1,44 @@
 package app.dto;
 
 import java.io.Serializable;
+import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
 
 public class EmployeeRequest implements Serializable {
 
+	private static final long serialVersionUID = 5303646281771996452L;
+
+	@NotBlank
+	private int id;
+	
+	@NotBlank
+	private String lastName;
+	
+	@NotBlank
+	private String firstName;
+	
+	@NotBlank
+	private String kanaLastName;
+	
+	@NotBlank
+	private String kanaFirstName;
+	
+	@NotBlank
+	private String age;
+	
+	@NotBlank
+	private String phone;
+	
+	@NotBlank
+	private String email;
+
+	@NotBlank
+	private Date createDate;
+	
+	@NotBlank
+	private Date updateDate;
+	
+	@NotBlank
+	private Date deleteDate;
 }
