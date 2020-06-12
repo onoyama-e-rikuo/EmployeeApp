fa954690aa5c32872d9313349982a4ef08f05e3c
onoyama-e-rikuo <evang00dd@gmail.com>
2020-06-12 11:45:31 +0900

    コントローラー作成

--- src/main/java/app/controller/EmployeeController.java
+++ src/main/java/app/controller/EmployeeController.java
@@ -1,13 +1,20 @@
 package app.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import app.dto.EmployeeUpdateRequest;
 import app.entity.Employee;
 import app.service.EmployeeService;
 
@@ -30,6 +37,20 @@ public class EmployeeController {
 		model.addAttribute("employeeDetails",employee);
 		return "details";
 	}
-	
-	
+
+	@PostMapping("/employee/update")
+	String update(@Validated @ModelAttribute EmployeeUpdateRequest employeeUpdateRequest, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			List<String> errorList = new ArrayList<String>();
+			for (ObjectError error : result.getAllErrors()) {
+				errorList.add(error.getDefaultMessage());
+			}
+			
+			model.addAttribute("validationError", errorList);
+			return "employee/edit";
+		}
+		
+		employeeService.update(employeeUpdateRequest);
+		return String.format("redirect:/employee/%d", employeeUpdateRequest.getId());
+	}
 }
