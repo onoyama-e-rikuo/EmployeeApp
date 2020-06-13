7a92656b2717815f6a478b336e2f28a10de1f59a
onoyama-e-rikuo <evang00dd@gmail.com>
2020-06-13 21:43:18 +0900

    repositoryにメソッド追加

--- src/main/java/app/repository/EmployeeRepository.java
+++ src/main/java/app/repository/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import app.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+	List<Employee> findByLastName(String lastName);
 }
