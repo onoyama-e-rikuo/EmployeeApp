c13f9f4be83ebc8fca6d433939e04d25b214429f
onoyama-e-rikuo <evang00dd@gmail.com>
2020-06-13 21:46:48 +0900

    dto追加

---
+++ src/main/java/app/dto/EmployeeForm.java
@@ -0,0 +1,12 @@
+package app.dto;
+
+import java.io.Serializable;
+
+import lombok.Data;
+@Data
+public class EmployeeForm implements Serializable {
+
+	private static final long serialVersionUID = 5436231166497005019L;
+
+	private String lastName;
+}
